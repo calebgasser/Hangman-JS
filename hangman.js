8793c6e92adc503d70a6df51016d46931a4ccdf4
Caleb Gasser <calebgasser@gmail.com>
2017-07-08 14:19:26 -0500

    background particale effect working.

--- hangman.js
+++ hangman.js
@@ -11,6 +11,94 @@ var fps = 1000 / 60;
 var glass = new Image();
 glass.src = './assets/images/glass_texture_clear.png';
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+var background = {
+	canvas: {},
+	ctx: {},
+	star: [],
+	particles: [],
+	numOfParticles: 100,
+	particleMaxLive: 200,
+	particleMinLive: 100,
+
+	init: function(){
+		background.canvas = document.getElementById('backgroundCanvas');
+		background.ctx = background.canvas.getContext('2d');
+		// background.ctx.canvas.width = window.innerWidth;
+		// background.ctx.canvas.height = window.innerHeight;
+		background.star[0] = new Image();
+		background.star[1] = new Image();
+		background.star[0].src = './assets/images/sparkle1.png';
+		background.star[1].src = './assets/images/sparkle2.png';
+	},
+	update: function(){
+		background.particalUpdate();
+		background.particalPopulate();
+		background.particaleDraw();
+	},
+	createParticle: function(x,y,life){
+		return {
+			posX: x,
+			posY: y,
+			lifeTime: life,
+			animReset: life/10,
+			anim: life/10,
+			currentFrame: 0,
+			frames: [background.star[0],background.star[1]]
+		}
+	},
+	particalUpdate: function(){
+		for(var i = 0; i < background.particles.length; i++){
+			background.particles[i].lifeTime -= 1;
+			background.particles[i].anim -= 1;
+			console.log(background.particles[i].anim);
+			if(background.particles[i].anim <= 0){
+				if(background.particles[i].currentFrame >= background.particles[i].frames.length-1){
+					background.particles[i].currentFrame = 0;
+				} else {
+					background.particles[i].currentFrame += 1;
+				}
+				background.particles[i].anim = background.particles[i].animReset;
+			}
+			if(background.particles[i].lifeTime <= 0){
+				background.particles.splice(i,1);
+			}
+		}
+	},
+	particalPopulate: function(){
+		if(background.particles.length <= background.numOfParticles){
+			for(i = 0; i < background.numOfParticles - background.particles.length; i++){
+				var position = background.getRandomPosition();
+				background.particles.push(background.createParticle(
+					position[0],
+					position[1],
+					getRandomIntInclusive(background.particleMinLive,background.particleMaxLive)
+					));
+			}
+		}
+
+	},
+	particaleDraw: function(){
+		background.ctx.clearRect(0, 0, background.canvas.width, background.canvas.height);
+		for(var i = 0; i < background.particles.length; i++){
+			background.ctx.drawImage(
+				background.particles[i].frames[background.particles[i].currentFrame],
+				background.particles[i].posX,
+				background.particles[i].posY,
+				64,
+				64)
+		}
+	},
+	getRandomPosition: function(){
+		return [getRandomIntInclusive(0,background.canvas.width),getRandomIntInclusive(0,background.canvas.height)];
+	}
+}
+
 var game = {
 	canvas: {},
 	ctx: {},
@@ -110,8 +198,10 @@ var game = {
 }
 
 $(function(){
+	background.init();
 	game.init();
 
+	setInterval(background.update,1000/30);
 	setInterval(game.update, fps);
 	$('#sound').on('click',function(){
 		console.log(backgroudMusic);
